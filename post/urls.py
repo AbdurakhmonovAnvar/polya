408e060546e0d096ac084d81b4d2f2eed6dd4239
AbdurakhmonovAnvar <anvardev888@gmail.com>
2025-05-20 22:33:26 +0500

    22:23/20/05/2025

--- post/urls.py
+++ post/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('api/polya/get-my-polyas',GetPolya.as_view(),name='get-my-polya'),
     path('api/polya/update/int:polya_id>',UpdatePolya.as_view(),name='update-polya'),
     path('api/polya/get-by-id-polya/<int:id>',GetPolyaById.as_view(),name='get-by-id-polya'),
+
+
 ]
