6b91fac55c41a89282b5cb1d68365e3c52456aba
AbdurakhmonovAnvar <anvardev888@gmail.com>
2025-05-23 11:53:21 +0500

    11:53/23/05/2025

--- polya/settings.py
+++ polya/settings.py
@@ -50,9 +50,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',  # Bu barcha APIlar uchun
-    ]
+
 }
 AUTH_USER_MODEL = 'user.User'
 
