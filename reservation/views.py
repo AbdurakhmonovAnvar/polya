5813d35267c1bc2bd3f4f0ad177554f17fd97a04
AbdurakhmonovAnvar <anvardev888@gmail.com>
2025-05-23 17:37:25 +0500

    17:37/23/05/2025

--- reservation/views.py
+++ reservation/views.py
@@ -13,19 +13,26 @@ class CreateReservation(APIView):
 
     def post(self, request, polya_id):
         serializer = CreateReservationSerializer(data=request.data)
+
+        # Polya tekshiruv
         try:
             polya = Polya.objects.get(pk=polya_id)
         except Polya.DoesNotExist:
             return Response({'message': 'Bunday polya topilmadi!'}, status=status.HTTP_404_NOT_FOUND)
-        try:
-            reservation = Reservation.objects.get(start_time=request.data['start_time'])
-        except Reservation.DoesNotExist:
-            # return Response({'message': 'Bu vaqtda bron qilingan'}, status=status.HTTP_400_BAD_REQUEST)
-            if serializer.is_valid():
-                start_time = serializer.validated_data.get('start_time')
-                serializer.save(start_time=start_time, customer=request.user, polya=polya)
-                return Response({'message': 'reservation saqlandi!'}, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        # Ma'lumot to'g'riligi tekshiriladi
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        start_time = serializer.validated_data.get('start_time')
+
+        # Belgilangan vaqtga rezervatsiya mavjudmi?
+        if Reservation.objects.filter(start_time=start_time, polya=polya).exists():
+            return Response({'message': 'Bu vaqtda bron qilingan!'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Saqlash
+        serializer.save(customer=request.user, polya=polya)
+        return Response({'message': 'Reservation saqlandi!'}, status=status.HTTP_201_CREATED)
 
 
 class GetMyReservation(APIView):
