136e429010c22bb2738c394bba5157bb39c85e3b
AbdurakhmonovAnvar <anvardev888@gmail.com>
2025-05-23 17:27:44 +0500

    17:27/23/05/2025

--- user/apps.py
+++ user/apps.py
@@ -1,22 +1,10 @@
-from django.apps import AppConfig
-from django.db.utils import OperationalError, ProgrammingError
-from django.core.exceptions import ObjectDoesNotExist
+# user/apps.py
 
+from django.apps import AppConfig
 
 class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user'
 
     def ready(self):
-        from django.contrib.auth import get_user_model
-        User = get_user_model()
-
-        try:
-            if not User.objects.filter(phone_number='+998900000101').exists():
-                User.objects.create_superuser(
-                    phone_number='+998900000101',
-                    password='admin1234',
-                    role='admin'
-                )
-        except (OperationalError, ObjectDoesNotExist, ProgrammingError):
-            pass
+        import user.signals
---
+++ user/signals.py
@@ -0,0 +1,15 @@
+# user/signals.py
+
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+
+@receiver(post_migrate)
+def create_default_admin(sender, **kwargs):
+    User = get_user_model()
+    if not User.objects.filter(phone_number='+998900000101').exists():
+        User.objects.create_superuser(
+            phone_number='+998900000101',
+            password='admin1234',
+            role='admin'
+        )
