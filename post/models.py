603cacdc1a84ee19c13c3c10c24d7a5bf7a76678
AbdurakhmonovAnvar <anvardev888@gmail.com>
2025-05-26 01:06:29 +0500

    01:06/26/05/2025

--- post/models.py
+++ post/models.py
@@ -2,20 +2,6 @@ from django.db import models
 from user.models import User
 
 
-class Polya(models.Model):
-    address = models.CharField(max_length=255, null=False)
-    locations = models.TextField()
-    images_path = models.TextField(blank=True)
-    type = models.CharField(max_length=30, choices=[('Futsal', 'futsal'), ('Lawn', 'lawn')], default='Lawn')
-    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='polya_created')
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    craeted_at = models.DateTimeField(auto_now_add=True)
-    status = models.BooleanField(default=True)
-
-    class Meta:
-        db_table = 'polya'
-
-
 class Region(models.Model):
     name = models.CharField(max_length=50)
 
@@ -30,4 +16,18 @@ class Street(models.Model):
     class Meta:
         db_table = 'street'
 
-# ass
+
+class Polya(models.Model):
+    address = models.CharField(max_length=255, null=False)
+    locations = models.TextField()
+    images_path = models.TextField(blank=True)
+    type = models.CharField(max_length=30, choices=[('Futsal', 'futsal'), ('Lawn', 'lawn')], default='Lawn')
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='polya_created')
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    street = models.ForeignKey(Street, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    craeted_at = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'polya'
