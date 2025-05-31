75230a39daa9c97e88e433574a0067d24fb4a4c6
AbdurakhmonovAnvar <anvardev888@gmail.com>
2025-05-31 15:27:17 +0500

    15:27/31/05/2025

--- polya/settings.py
+++ polya/settings.py
@@ -145,16 +145,14 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = False
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 import os
 
 STATIC_URL = '/static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
